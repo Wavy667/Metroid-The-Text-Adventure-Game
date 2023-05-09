f5db0b4685b30bc326e10a5fe3229270a1e0cca4
Wavy667 <15330717-Wavy667@users.noreply.replit.com>
2023-05-09 04:27:51 +0000

    Implement basic framework
    From here it should be easy to develop the game

--- main.py
+++ main.py
@@ -1,3 +1,55 @@
 import sys
 import random
 import time
+import colorama
+
+global gMain
+
+gCurrentScene = 1
+gMain = True
+pBeams = ["power"]
+
+"""
+pGetInput:
+Main input routine 
+"""
+def pGetInput() :
+	global pInput
+	
+	pInputBreak = True
+
+	while pInputBreak :
+		pInput = input("> ").lower()
+		
+		if pInput == "stats" :
+			print("Your beams:")
+			for x in pBeams :
+				print(x.title())
+		elif pInput == "exit" :
+			print("See you next mission!")
+			sys.exit()
+		else :
+			return pInput
+
+"""
+Main loop
+"""
+while True :
+	# scene 1
+	if gCurrentScene == 1:
+		print("""
+ test scene
+ 	""")
+		gSceneActions = ["left", "right"]
+		
+		pGetInput()
+
+		print("You typed", pInput)
+
+		# Scene specific inputs
+		match pInput :
+			case "left" :
+				gCurrentScene = 2
+			case "right" :
+				gCurrentScene = 2
+			
